e25f02c34b50ac7e71bad9413afee90ab1301000
mianzainch000 <mianzainch000@gmail.com>
2025-08-20 17:03:42 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<!-- For Deployment
+Mongo_Url or Port
+Index.js or config/db.js mai jo code hai wo srif vercel par project upload karny ka liye hai
+Index.js ko serveless b aisy liye baniya hai -->
---
+++ src/route/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controller/userController");
+
+router.post("/signupMyPersonalData", userController.signup);
+router.post("/login", userController.login);
+router.post("/forgotPassword", userController.forgotPassword);
+router.post("/resetPassword/:tokenEmail", userController.resetPassword);
+module.exports = router;
